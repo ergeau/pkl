16fafd8a36c535e50c3258a9c7b5c3eb295f9d57
rg <rehangilangf@gmail.com>
2024-12-19 21:27:42 +0700

    commit

--- resources/views/about/about.blade.php
+++ resources/views/about/about.blade.php
@@ -35,20 +35,7 @@
 <body>
     <div class="container">
         <div class="content">
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates earum distinctio possimus pariatur aperiam repudiandae vitae expedita illo neque, aspernatur id consectetur facilis. Excepturi ut veritatis rerum neque, eos recusandae?</p>
-            <!-- <h1>About Us</h1> -->
-            <!-- <p>Raihan Gilang Firdausy</p> -->
-            <!-- <p>Raihan Gilang Firdausy adalah seorang mahasiswa yang saat ini sedang menjalani Praktik Kerja Lapangan (PKL) di Gamelab. Sebagai seorang yang memiliki minat besar dalam dunia teknologi dan pendidikan, Raihan memilih untuk bergabung dengan Gamelab guna mengembangkan keterampilannya dalam pembuatan game edukasi.
-            </p>
-            <h2>Pengalaman di Gamelab</h2>
-            <p>Selama masa PKL di Gamelab, Raihan terlibat dalam berbagai proyek pengembangan game yang berfokus pada pendidikan. Ia bekerja sama dengan tim profesional dalam menciptakan konten interaktif yang tidak hanya menghibur, tetapi juga memberikan nilai pendidikan yang tinggi. Beberapa proyek yang dikerjakan oleh Raihan melibatkan pengembangan game yang dirancang untuk membantu anak-anak belajar matematika, sains, dan bahasa dengan cara yang menyenangkan dan menarik.
-            </p>
-            <h2>Misi dan Visi</h2>
-            <p>Dengan pengalamannya di Gamelab, Raihan bercita-cita untuk terus mengembangkan game edukasi yang inovatif dan efektif. Ia percaya bahwa game edukasi memiliki potensi besar untuk merevolusi cara kita belajar dan mengajar. Melalui game, konsep-konsep yang kompleks dapat disampaikan dengan cara yang lebih mudah dipahami dan diingat oleh anak-anak.
-            </p>
-            <h2>Komitmen</h2>
-            <p>Raihan berkomitmen untuk terus belajar dan berinovasi dalam bidang teknologi pendidikan. Ia berharap dapat memberikan kontribusi positif bagi dunia pendidikan melalui karyanya di Gamelab dan di masa depan. Dengan semangat dan dedikasi yang tinggi, Raihan siap untuk menghadapi tantangan dan peluang yang ada di depan, dengan tujuan akhir untuk menciptakan pengalaman belajar yang lebih baik bagi generasi mendatang.
-            </p> -->
+            <p>Game ini merupakan game edukasi sederhana yang dibuat sebagai tugas akhir dari magang di Gamelab Indonesia. Game ini dibuat oleh Raihan Gilang Firdausy, dan dikembangkan menggunakan Phaser JS untuk mekanika permainan, sementara websitenya dibangun dengan menggunakan framework Laravel. Game ini bertujuan untuk memberikan pengalaman belajar yang menyenangkan dan interaktif bagi para penggunanya.</p>
         </div>
     </div>
 </body>
